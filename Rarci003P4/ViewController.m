c04c4c3d74fd7e028f9e5002c9fb622307979c19
rarciniegas <rarci003@fiu.edu>
2015-10-07 14:38:39 -0400

    Initial Commit

---
+++ Rarci003P4/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Rarci003P4
+//
+//  Created by roberto arciniegas on 10/7/15.
+//  Copyright (c) 2015 roberto arciniegas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
